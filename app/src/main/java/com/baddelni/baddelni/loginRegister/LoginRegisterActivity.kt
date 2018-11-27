cb8c507863d755be69c4c9924def1684c896b234
Dilwar Singh <dilwarsingh2706@gmail.com>
2018-11-27 09:25:48 +0530

    Initial commit

---
+++ app/src/main/java/com/baddelni/baddelni/loginRegister/LoginRegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.baddelni.baddelni.loginRegister
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.baddelni.baddelni.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
