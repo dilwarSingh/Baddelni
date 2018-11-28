56a05d19aa4ea2baeba0b5a33c3f4ca6d1ce4b90
Dilwar Singh <dilwarsingh2706@gmail.com>
2018-11-28 09:31:52 +0530

    ad

--- app/src/main/java/com/baddelni/baddelni/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/baddelni/baddelni/loginRegister/RegisterFragment.kt
@@ -7,9 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.baddelni.baddelni.R
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginText.setOnClickListener { (context as LoginRegisterActivity).changeContainerFragment(LoginFragment()) }
+        backBt.setOnClickListener { (context as LoginRegisterActivity).changeContainerFragment(EmailFragment()) }
+
+        male.setOnClickListener { male.isChecked = true; female.isChecked = false; }
+        female.setOnClickListener { female.isChecked = true; male.isChecked = false; }
+    }
+
+
 }
